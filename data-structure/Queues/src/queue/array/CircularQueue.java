4a57536d92e369a6b35c8a261ecfda6a15365ffb
agribeau <hkissbigapple@163.com>
2018-08-22 12:09:34 +0800

    feat(circular queue): Initialize circular queue

---
+++ data-structure/Queues/src/queue/array/CircularQueue.java
@@ -0,0 +1,28 @@
+package queue.array;
+
+public class CircularQueue<E>implements Queue<E> {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peekFront() {
+        return null;
+    }
+}
