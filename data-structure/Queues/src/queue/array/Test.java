6e7d6cab93cdf27f213f7b44f9e4320fd8e60fb2
agribeau <hkissbigapple@163.com>
2018-08-23 14:28:46 +0800

    feat(circular queue): Add test

--- data-structure/Queues/src/queue/array/Test.java
+++ data-structure/Queues/src/queue/array/Test.java
@@ -1,4 +1,16 @@
 package queue.array;
 
 public class Test {
+    public static void main(String args[]) {
+        CircularQueue<Integer> queue = new CircularQueue<>();
+        for (int i = 0; i < 5; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
 }
