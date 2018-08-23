e08e0e9c7a83af2b2b9d9334a4b6d7c656127f34
agribeau <hkissbigapple@163.com>
2018-08-23 10:07:41 +0800

    feat(circular queue): Add constructor function

--- data-structure/Queues/src/queue/array/CircularQueue.java
+++ data-structure/Queues/src/queue/array/CircularQueue.java
@@ -1,6 +1,22 @@
 package queue.array;
 
-public class CircularQueue<E>implements Queue<E> {
+public class CircularQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    @SuppressWarnings("unchecked")
+    public CircularQueue(int capacity) {
+        this.data = (E[]) new Object[capacity];
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
     @Override
     public int getSize() {
         return 0;
