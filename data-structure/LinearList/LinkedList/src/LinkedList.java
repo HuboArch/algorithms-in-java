3cac39cfc4cf25833ee3da3315a6938101c0177f
agribeau <hkissbigapple@163.com>
2018-08-24 11:16:35 +0800

    feat(linked list): Add private internal class Node

--- data-structure/LinearList/LinkedList/src/LinkedList.java
+++ data-structure/LinearList/LinkedList/src/LinkedList.java
@@ -1,3 +1,24 @@
 public class LinkedList<E> {
 
+    /**
+     * 以私有内部类的形式定义结点类型
+     */
+    private class Node {
+        public E    e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
 }
