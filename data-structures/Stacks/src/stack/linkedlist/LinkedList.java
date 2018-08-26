541acf8f3ca472e88165c007ada52bfb0c32abf9
agribeau <hkissbigapple@163.com>
2018-08-27 00:11:25 +0800

    feat(linkedList stack): Create an stack with linkedList

---
+++ data-structures/Stacks/src/stack/linkedlist/LinkedList.java
@@ -0,0 +1,4 @@
+package stack.linkedlist;
+
+public class LinkedList<E> {
+}
---
+++ data-structures/Stacks/src/stack/linkedlist/LinkedListStack.java
@@ -0,0 +1,28 @@
+package stack.linkedlist;
+
+public class LinkedListStack<E> implements Stack<E> {
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
