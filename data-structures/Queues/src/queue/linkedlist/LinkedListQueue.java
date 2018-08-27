cda1760a240549bba5dbaff99eb5308e9fd6bccf
agribeau <hkissbigapple@163.com>
2018-08-27 15:44:44 +0800

    feat(linkedList queue): Reimplement the queue based on linked list

---
+++ data-structures/Queues/src/queue/linkedlist/LinkedListQueue.java
@@ -0,0 +1,40 @@
+package queue.linkedlist;
+
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private LinkedList<E> list;
+
+    public LinkedListQueue() {
+        this.list = new LinkedList<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        list.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public E peekHead() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        return "Queue: \n" + list;
+    }
+}
