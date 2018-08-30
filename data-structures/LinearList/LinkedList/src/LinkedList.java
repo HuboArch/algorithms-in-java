afd318a4e6190afe5af2c5778db32be851d82fe3
agribeau <hkissbigapple@163.com>
2018-08-30 09:20:38 +0800

    refactor(linked list): Change the access permission of Node fields and constructor

--- data-structures/LinearList/LinkedList/src/LinkedList.java
+++ data-structures/LinearList/LinkedList/src/LinkedList.java
@@ -5,18 +5,18 @@ public class LinkedList<E> {
      */
     private class Node {
         public E    e;
-        public Node next;
+        Node next;
 
-        public Node(E e, Node next) {
+        Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
 
-        public Node(E e) {
+        Node(E e) {
             this(e, null);
         }
 
-        public Node() {
+        Node() {
             this(null, null);
         }
 
