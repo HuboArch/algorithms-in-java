dd589f74882bdf8dd96dcae6e1d716e20969482a
agribeau <hkissbigapple@163.com>
2018-09-11 09:33:19 +0800

    feat(binary search tree): Create binary search tree

---
+++ data-structures/Trees/src/BinarySearchTree.java
@@ -0,0 +1,30 @@
+public class BinarySearchTree<E extends Comparable<E>> {
+
+    private class Node {
+        E    e;
+        Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int  size;
+
+    public BinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
