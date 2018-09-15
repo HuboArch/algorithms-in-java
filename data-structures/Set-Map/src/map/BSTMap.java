6143701a8d1ce788e4ff09229b1bb2b8e1d1935d
agribeau <hkissbigapple@163.com>
2018-09-15 12:50:29 +0800

    feat(map): Create map based on binary search tree

---
+++ data-structures/Set-Map/src/map/BSTMap.java
@@ -0,0 +1,60 @@
+package map;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private class Node {
+        K    key;
+        V    value;
+        Node left, right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int  size;
+
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return false;
+    }
+
+    @Override
+    public void add(K key, V value) {
+
+    }
+
+    @Override
+    public void remove(K key) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void set(K key, V value) {
+
+    }
+}
