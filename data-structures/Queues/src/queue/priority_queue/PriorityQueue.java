5c4ccd94d614b1f7f62e5122fce28ea6fbe6fc4b
agribeau <hkissbigapple@163.com>
2018-09-19 09:41:34 +0800

    feat(priority queue): Add method of getting index of the specified node

--- data-structures/Queues/src/queue/priority_queue/PriorityQueue.java
+++ data-structures/Queues/src/queue/priority_queue/PriorityQueue.java
@@ -19,4 +19,38 @@ public class PriorityQueue<E extends Comparable<E>> {
     public boolean isEmpty() {
         return data.isEmpty();
     }
+
+    /**
+     * 在完全二叉树的数组表示中，获取指定节点的父亲节点所在的索引
+     *
+     * @param idx 待查询的节点的在数组中的索引
+     * @return 父亲节点在数组中的索引
+     */
+    private int parent(int idx) {
+        if (idx < 0) {
+            throw new IllegalArgumentException("Index 0 does not have parent node.");
+        }
+
+        return (idx - 1) / 2;
+    }
+
+    /**
+     * 获取指定节点的左子节点在数组中的索引
+     *
+     * @param idx 待查询节点在数组中的索引
+     * @return 左子节点的索引
+     */
+    private int leftChild(int idx) {
+        return 2 * idx + 1;
+    }
+
+    /**
+     * 获取指定节点的右子节点在数组中的索引
+     *
+     * @param idx 待查询节点在数组中的索引
+     * @return 右子节点的索引
+     */
+    private int rightChild(int idx) {
+        return 2 * idx + 2;
+    }
 }
