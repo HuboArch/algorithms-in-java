a16506053db10ea578503d3169edba2491abc542
agribeau <hkissbigapple@163.com>
2018-09-20 11:31:49 +0800

    feat(max heap): Add methods of inserting an element into the heap

--- data-structures/Heaps/src/max_heap/MaxHeap.java
+++ data-structures/Heaps/src/max_heap/MaxHeap.java
@@ -56,4 +56,30 @@ public class MaxHeap<E extends Comparable<E>> {
         return 2 * idx + 2;
     }
 
+
+    public boolean add(E e) {
+        if (e == null) {
+            throw new NullPointerException("Can not insert null into the heap.");
+        }
+        int i = size();
+        if (i == 0) {
+            data.set(0, e);
+        } else {
+            siftUp(i - 1, e);
+        }
+        return true;
+    }
+
+    private void siftUp(int k, E x) {
+        while (k > 0) {
+            int parent = getParentIdx(k);
+            E   e      = data.get(parent);
+            if (x.compareTo(e) <= 0) {
+                break;
+            }
+            data.set(k, e);
+            k = parent;
+        }
+        data.set(k, x);
+    }
 }
