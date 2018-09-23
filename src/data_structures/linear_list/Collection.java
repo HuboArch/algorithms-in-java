d17b992391a85bac0239010b52a67974726207d3
agribeau <hkissbigapple@163.com>
2018-09-23 13:21:28 +0800

    feat: Create interface Collection

--- src/data_structures/linear_list/Collection.java
+++ src/data_structures/linear_list/Collection.java
@@ -1,4 +1,48 @@
 package data_structures.linear_list;
 
-public interface Collection {
+public interface Collection<E> {
+
+    /**
+     * 获取collection中元素的数量
+     *
+     * @return collection中元素的数量
+     */
+    int size();
+
+    /**
+     * 判断collection是否为空
+     *
+     * @return 如果为空，返回true
+     */
+    boolean isEmpty();
+
+    /**
+     * 判断collection中是否含有指定的元素
+     *
+     * @param o 待检测的元素
+     * @return 如果包含指定的元素，返回true
+     */
+    boolean contains(Object o);
+
+    /**
+     * 确保collection包含指定的元素
+     *
+     * @param e 被确保要添加进collection中的元素
+     * @return 如果该方法执行后，collection发生了变化，返回true
+     */
+    boolean add(E e);
+
+    /**
+     * 从collection中移除一个指定的元素(移除首次出现位置上的元素)
+     *
+     * @param o 待移除的元素
+     * @return 如果元素被成功移除，返回true
+     */
+    boolean remove(Object o);
+
+    /**
+     * 清空collection
+     */
+    void clear();
+
 }
--- src/data_structures/linear_list/List.java
+++ src/data_structures/linear_list/List.java
@@ -1,4 +1,92 @@
 package data_structures.linear_list;
 
-public interface List {
+public interface List<E> extends Collection<E> {
+
+    /**
+     * 获取指定索引位置上的元素
+     *
+     * @param index 索引
+     * @return 指定索引位置上的元素
+     */
+    E get(int index);
+
+    /**
+     * 替换指定索引位置上的元素
+     *
+     * @param index   索引
+     * @param element 待存储的元素
+     * @return 该索引位置上之前的元素
+     */
+    E set(int index, E element);
+
+    /**
+     * 在指定的索引位置插入元素，后面的元素向右移动一个索引位置
+     *
+     * @param index   待插入的索引位置
+     * @param element 待插入的元素
+     */
+    void add(int index, E element);
+
+    /**
+     * 移除指定索引位置上的元素，后面的元素向左移动一个索引位置
+     *
+     * @param index 带移除元素所在的索引位置
+     * @return 被移除的元素
+     */
+    E remove(int index);
+
+    /**
+     * 获取指定元素第一次出现所在的位置索引
+     *
+     * @param o 待查询的元素
+     * @return 返回元素第一次出现所在的索引，若不存在该元素，返回 -1
+     */
+    int indexOf(Object o);
+
+    /* ========= Extended Methods from Collection ========= */
+    /* =============== JUST FOR READABILITY =============== */
+
+    /**
+     * 获取list中元素的数量，如果list中的数量超过了Integer.MAX_VALUE,
+     * 返回Integer.MAX_VALUE
+     *
+     * @return list中元素的数量
+     */
+    int size();
+
+    /**
+     * 判断list是否为空
+     *
+     * @return list为空, 返回true
+     */
+    boolean isEmpty();
+
+    /**
+     * 判断list中是否包含指定的元素
+     *
+     * @param o 待检测的元素
+     * @return list中包含指定的元素，返回true
+     */
+    boolean contains(Object o);
+
+    /**
+     * 在list的尾部添加元素
+     *
+     * @param e 待添加的元素
+     * @return 方法执行后，list发生了变化，返回true
+     */
+    boolean add(E e);
+
+    /**
+     * 移除list中首次出现的指定元素
+     *
+     * @param o 待移除的元素
+     * @return 如果list中包含指定元素，返回true
+     */
+    boolean remove(Object o);
+
+    /**
+     * 清空list
+     */
+    void clear();
 }
