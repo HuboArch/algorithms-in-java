d5dc96994d300de1297417104b38efb0b5994e0f
agribeau <hkissbigapple@163.com>
2018-09-24 22:52:10 +0800

    fix(max heap): Fix field data

--- src/data_structures/heap/MaxHeap.java
+++ src/data_structures/heap/MaxHeap.java
@@ -15,7 +15,7 @@ public class MaxHeap<E extends Comparable<E>> {
     }
 
     public int size() {
-        return data.getSize();
+        return data.size();
     }
 
     public boolean isEmpty() {
@@ -78,7 +78,7 @@ public class MaxHeap<E extends Comparable<E>> {
     private void siftUp(int k, E x) {
         while (k > 0) {
             int parent = getParentIdx(k);
-            E   e      = data.get(parent);
+            E e = data.get(parent);
             if (x.compareTo(e) <= 0) {
                 break;
             }
@@ -94,7 +94,7 @@ public class MaxHeap<E extends Comparable<E>> {
      * @return 最大的元素
      */
     public E findMax() {
-        if (data.getSize() == 0) {
+        if (data.size() == 0) {
             throw new IllegalArgumentException("Can not find the maximum element from an empty heap.");
         }
 
@@ -108,7 +108,7 @@ public class MaxHeap<E extends Comparable<E>> {
      */
     public E extractMax() {
         E ret = findMax();
-        E e   = data.removeLast();
+        E e = data.remove(data.size() - 1);
         siftDown(0, e);
         return ret;
     }
@@ -118,7 +118,7 @@ public class MaxHeap<E extends Comparable<E>> {
         int half = size >>> 1;
         while (k < half) {
             int child = getLeftChildIdx(k);
-            E   c     = data.get(child);
+            E c = data.get(child);
             int right = child + 1;
             if (right < size && c.compareTo(data.get(right)) < 0) {
                 c = data.get(child = right);
