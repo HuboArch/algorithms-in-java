63274a6a1af01dfb0d4145cd7e0d810e0c1d53d3
agribeau <hkissbigapple@163.com>
2018-09-25 16:35:11 +0800

    feat(linked list): Add method of unlinking the specified node

--- src/data_structure/linear_list/LinkedList.java
+++ src/data_structure/linear_list/LinkedList.java
@@ -111,4 +111,34 @@ public class LinkedList<E> implements List<E> {
 
         return element;
     }
+
+    /**
+     * 移除指定的非空节点
+     */
+    private E unlink(Node<E> x) {
+        // assert x != null
+        final E element = x.item;
+        final Node<E> prev = x.prev;
+        final Node<E> next = x.next;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+
+        size--;
+
+        return element;
+    }
 }
