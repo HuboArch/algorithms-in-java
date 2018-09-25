7748c1507c2b54af830ff72404bf6878b2355587
agribeau <hkissbigapple@163.com>
2018-09-25 14:40:56 +0800

    feat(linked list): Add method of linking the specified element as first one

--- src/data_structure/linear_list/LinkedList.java
+++ src/data_structure/linear_list/LinkedList.java
@@ -6,7 +6,7 @@ public class LinkedList<E> {
         Node<E> prev;
         Node<E> next;
 
-        public Node(E item, Node<E> prev, Node<E> next) {
+        Node(Node<E> prev, E item, Node<E> next) {
             this.item = item;
             this.prev = prev;
             this.next = next;
@@ -18,5 +18,20 @@ public class LinkedList<E> {
     private int size = 0;
 
     public LinkedList() {
+    }
+
+    /**
+     * 在链表首部添加节点
+     */
+    private void linkFirst(E e) {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null) {
+            last = newNode;
+        } else {
+            f.prev = newNode;
+        }
+        size++;
     }
 }
