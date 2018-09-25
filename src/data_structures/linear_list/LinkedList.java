acdc0b6448413ba3212ed3fe2736a95d19abeb5e
agribeau <hkissbigapple@163.com>
2018-09-25 11:29:24 +0800

    feat(linear list): Create LinkedList

---
+++ src/data_structures/linear_list/LinkedList.java
@@ -0,0 +1,22 @@
+package data_structures.linear_list;
+
+public class LinkedList<E> {
+    private static class Node<E> {
+        E item;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(E item, Node<E> prev, Node<E> next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node<E> first;
+    private Node<E> last;
+    private int size = 0;
+
+    public LinkedList() {
+    }
+}
