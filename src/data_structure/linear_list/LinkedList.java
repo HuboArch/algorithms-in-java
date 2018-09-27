3186933f14b46b6c9bf191b2b8d2df2b4bdfe829
agribeau <hkissbigapple@163.com>
2018-09-27 09:50:27 +0800

    feat(linked list): Add method of getting the node at the specified element index

--- src/data_structure/linear_list/LinkedList.java
+++ src/data_structure/linear_list/LinkedList.java
@@ -283,4 +283,25 @@ public class LinkedList<E> implements List<E> {
             throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
         }
     }
+
+    /**
+     * 获取指定索引上的节点
+     */
+    private Node<E> node(int index) {
+        // assert isElementIndex(index);
+
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++) {
+                x = x.next;
+            }
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--) {
+                x = x.next;
+            }
+            return x;
+        }
+    }
 }
