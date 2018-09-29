5ad8677bbf97f5a4e9830e65043c5ee5faa21637
agribeau <hkissbigapple@163.com>
2018-09-29 11:02:05 +0800

    feat(min heap): Add constructor function

--- src/data_structure/heap/MinHeap.java
+++ src/data_structure/heap/MinHeap.java
@@ -1,4 +1,25 @@
 package data_structure.heap;
 
-public class MinHeap {
+import data_structure.linear_list.ArrayList;
+
+import java.util.Comparator;
+
+public class MinHeap<E extends Comparable<E>> {
+    private ArrayList<E> data;
+    private int size = 0;
+    private final Comparator<? super E> comparator;
+
+    public MinHeap() {
+        this(null);
+    }
+
+    public MinHeap(Comparator<? super E> comparator) {
+        this.data = new ArrayList<>();
+        this.comparator = comparator;
+    }
+
+    public MinHeap(int initialCapacity, Comparator<? super E> comparator) {
+        this.data = new ArrayList<>(initialCapacity);
+        this.comparator = comparator;
+    }
 }
