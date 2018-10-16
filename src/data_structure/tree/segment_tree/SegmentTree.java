744a38fd66629f0d52f1887ea6e122977b2b618e
agribeau <hkissbigapple@163.com>
2018-10-16 12:44:34 +0800

    feat(segment tree): Add constructor function

--- src/data_structure/tree/segment_tree/SegmentTree.java
+++ src/data_structure/tree/segment_tree/SegmentTree.java
@@ -1,4 +1,17 @@
 package data_structure.tree.segment_tree;
 
-public class SegmentTree {
+public class SegmentTree<E> {
+    private E[] tree;
+    private E[] data;
+
+    @SuppressWarnings("unchecked")
+    public SegmentTree(E[] arr) {
+        data = (E[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+
+        tree = (E[]) new Object[4 * arr.length];
+    }
+
 }
