f8cd519d37030ab9e016a87d668e6a2fdb3f183c
Huubo <hkissbigapple@163.com>
2019-01-09 14:53:57 +0800

    feat(min heap): Implement min heap

--- src/data_structure/heap/MinHeap.java
+++ src/data_structure/heap/MinHeap.java
@@ -1,15 +1,26 @@
 package data_structure.heap;
 
-import data_structure.linear_list.ArrayList;
-
 import java.util.Comparator;
 
-public class MinHeap<E extends Comparable<E>> {
-    private ArrayList<E> data;
+/**
+ * @author Hubo
+ * Project: algorithms-in-java
+ * Time: 2019/1/9 14:47
+ * Email: hkissbigapple@163.com
+ */
+public class MinHeap<E> {
+
+    private Object[] elements;
+
+    private int size;
+
     private final Comparator<? super E> comparator;
 
-    public MinHeap() {
-        this(null);
+    private static final int DEFAULT_INITIAL_CAPACITY = 11;
+
+    public MinHeap(int initialCapacity, Comparator<? super E> comparator) {
+        this.elements = new Object[initialCapacity];
+        this.comparator = comparator;
     }
 
     public MinHeap(int initialCapacity) {
@@ -17,250 +28,135 @@ public class MinHeap<E extends Comparable<E>> {
     }
 
     public MinHeap(Comparator<? super E> comparator) {
-        this.data = new ArrayList<>();
-        this.comparator = comparator;
+        this(DEFAULT_INITIAL_CAPACITY, comparator);
     }
 
-    public MinHeap(int initialCapacity, Comparator<? super E> comparator) {
-        this.data = new ArrayList<>(initialCapacity);
-        this.comparator = comparator;
+    public MinHeap() {
+        this(DEFAULT_INITIAL_CAPACITY, null);
     }
 
-    public int size() {
-        return data.size();
+    private void grow() {
+        final Object[] newElementData = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newElementData, 0, elements.length);
+        elements = newElementData;
     }
 
-    public boolean isEmpty() {
-        return data.isEmpty();
-    }
-
-    /**
-     * 判断是否包含指定元素
-     *
-     * @param o 待查询元素
-     * @return 包含指定的元素，返回true
-     */
-    public boolean contains(Object o) {
-        return data.contains(o);
-    }
-
-    public E findMin() {
-        return data.get(0);
+    @SuppressWarnings("unchecked")
+    private int compare(E a, E b) {
+        if (comparator != null) {
+            return comparator.compare(a, b);
+        } else {
+            return ((Comparable<? super E>) a).compareTo(b);
+        }
     }
 
     /**
      * 向堆中添加元素
      *
-     * @param e 待添加的元素
+     * @param element 待添加元素
      */
-    public boolean offer(E e) {
-        if (e == null) {
-            throw new NullPointerException();
+    public void add(E element) {
+        // assert element is not null
+        if (elements.length == size) { // 存储小顶堆数据项的数组已满
+            grow();
         }
 
-        data.add(e);
+        elements[++size] = element;
 
-        // 为维持最小堆的特性，新添加的元素需要上浮
-        int i = size() - 1;
-        if (i != 0) {
-            siftUp(i, e);
-        }
-
-        return true;
-    }
-
-    public void clear() {
-        data.clear();
-    }
-
-    /**
-     * 移除指定元素
-     *
-     * @param o 待移除元素
-     */
-    public boolean remove(Object o) {
-        int i = data.indexOf(o);
-        if (i == -1) {
-            return false;
-        } else {
-            removeAt(i);
-            return true;
+        // 自底向上进行堆化操作
+        int index = size - 1;
+        if (index != 0) {
+            siftUp(index, element);
         }
     }
 
     /**
-     * 移除最小堆的首节点
+     * 自底向上的堆化操作
      *
-     * @return 首节点元素
+     * @param k       上浮元素在数组中的索引，通常是 this.size - 1
+     * @param element 待上浮的元素
      */
+    @SuppressWarnings("unchecked")
+    private void siftUp(int k, E element) {
+
+        while (k > 0) {
+            int parent = parentIndex(k);
+            E p = (E) elements[parent];
+
+            if (compare(element, p) >= 0) {
+                break;
+            }
+            elements[k] = parent;
+
+            k = parent;
+        }
+
+        elements[k] = element;
+    }
+
+    /**
+     * 取出堆顶元素
+     *
+     * @return 堆顶元素
+     */
+    @SuppressWarnings("unchecked")
     public E poll() {
-        int s = data.size();
-        if (s == 0) {
+        if (size == 0) {
             return null;
         }
+        final E min = (E) elements[0];
 
-        E result = data.get(0);
-        E x = data.remove(s - 1);
-        siftDown(0, x);
+        elements[0] = elements[--size];
+        elements[size + 1] = null;  // let GC do its work
 
-        return result;
+        // 自顶向下进行堆化操作
+        E element = (E) elements[0];
+        siftDown(0, element);
+
+        return min;
     }
 
     /**
-     * 获取最小堆首元素
+     * 5 --> 将子树节点中的最小值 3 移动到该位置
+     * /  \
+     * 3     7
+     * <p>
+     * 自顶向下的堆化操作
      *
-     * @return 首元素
+     * @param k       待下沉堆顶元素的数组索引，为 0
+     * @param element 待下沉的堆顶元素
      */
-    public E peek() {
-        return data.isEmpty() ? null : data.get(0);
+    @SuppressWarnings("unchecked")
+    private void siftDown(int k, E element) {
+
+        int half = size / 2;
+        while (k < half) {
+            int child = leftChildIndex(k);
+            E c = (E) elements[child];
+
+            int right = child + 1;
+            if (right < size && compare(c, (E) elements[right]) > 0) {
+                c = (E) elements[child = right];
+            }
+
+            if (compare(element, c) <= 0) {
+                break;
+            }
+            elements[k] = c;
+
+            k = child;
+        }
+
+        elements[k] = element;
     }
 
-    /*=============================================================*/
-    /*========================== 私有方法 ==========================*/
-    /*=============================================================*/
-
-    /**
-     * 在完全二叉树的数组表示中，获取指定节点的父亲节点所在的索引
-     *
-     * @param index 待查询的节点的在数组中的索引
-     * @return 父亲节点在数组中的索引
-     */
     private int parentIndex(int index) {
         return (index - 1) / 2;
     }
 
-    /**
-     * 获取指定节点的左子节点在数组中的索引
-     *
-     * @param index 待查询节点在数组中的索引
-     * @return 左子节点的索引
-     */
     private int leftChildIndex(int index) {
         return 2 * index + 1;
     }
 
-    /**
-     * 在指定的位置插入元素，之后，为了维持堆的特性，需要上移
-     * 插入的元素，直到元素大于或等于父节点中的值或者成为根节点
-     *
-     * @param k 待插入的位置
-     * @param x 待插入的元素
-     */
-    private void siftUp(int k, E x) {
-        if (comparator != null) {
-            siftUpUsingComparator(k, x);
-        } else {
-            siftUpComparable(k, x);
-        }
-    }
 
-    private void siftUpComparable(int k, E x) {
-        while (k > 0) {
-            int parent = parentIndex(k);
-            E e = data.get(parent);
-
-            if (x.compareTo(e) >= 0) {
-                break;
-            }
-
-            data.set(k, e);
-            k = parent;
-        }
-        data.set(k, x);
-    }
-
-    private void siftUpUsingComparator(int k, E x) {
-        while (k > 0) {
-            int parent = parentIndex(k);
-            E e = data.get(parent);
-
-            if (comparator.compare(x, e) >= 0) {
-                break;
-            }
-
-            data.set(k, e);
-            k = parent;
-        }
-        data.set(k, x);
-    }
-
-    /**
-     * 在指定的位置插入元素，之后，为了维持堆的特性，需要下移
-     * 插入的元素，直到元素小于或等于子节点中的最小值或是变为叶子节点
-     *
-     * @param k 待插入的位置
-     * @param x 待插入的元素
-     */
-    private void siftDown(int k, E x) {
-        if (comparator != null) {
-            siftDownUsingComparator(k, x);
-        } else {
-            siftDownComparable(k, x);
-        }
-    }
-
-    private void siftDownComparable(int k, E x) {
-        int size = size();
-        int half = size >>> 1;
-        while (k < half) {                 // 循环至没有子节点，2*k + 1 < size ==> k < size/2 - 1/2 ==> k < size/2
-            int child = leftChildIndex(k); // 假设左子节点中值最小
-            E c = data.get(child);
-
-            int right = child + 1;
-            if (right < size && c.compareTo(data.get(right)) > 0) {
-                c = data.get(child = right);
-            }
-
-            if (x.compareTo(c) <= 0) {
-                break;
-            }
-
-            data.set(k, c);
-            k = child;
-        }
-        data.set(k, x);
-    }
-
-    private void siftDownUsingComparator(int k, E x) {
-        int size = size();
-        int half = size >>> 1;
-        while (k < half) {                 // 循环至没有子节点，2*k + 1 < size ==> k < size/2 - 1/2 ==> k < size/2
-            int child = leftChildIndex(k); // 假设左子节点中值最小
-            E c = data.get(child);
-
-            int right = child + 1;
-            if (right < size && comparator.compare(c, data.get(right)) > 0) {
-                c = data.get(child = right);
-            }
-
-            if (comparator.compare(x, c) <= 0) {
-                break;
-            }
-
-            data.set(k, c);
-            k = child;
-        }
-        data.set(k, x);
-    }
-
-    /**
-     * 移除指定索引的元素
-     *
-     * @param i 待移除元素的索引
-     */
-    private void removeAt(int i) {
-        int s = data.size() - 1;
-
-        if (i == s) {
-            data.remove(i);
-        } else {
-            E moved = data.get(s);
-            data.set(s, null);
-
-            siftDown(i, moved);
-            if (data.get(i) == moved) {
-                siftUp(i, moved);
-            }
-        }
-    }
 }
--- src/data_structure/queue/Deque.java
+++
@@ -1,4 +0,0 @@
-package data_structure.queue;
-
-public interface Deque {
-}
